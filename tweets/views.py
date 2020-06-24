9cdf28be3401c707c0dab328ad60ddc9b39cf7e5
Александр Яцевский <sashayatsevskiy@ukr.net>
2020-06-24 19:49:41 +0300

    add forms

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,29 @@
 import random
 
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Tweet
+from .forms import TweetForm
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print( args, kwargs)
     return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next")
+    print("next_url: ", next)
+    if form.is_valid(): 
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        if next_url != None:
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, 'components/forms.html', context={"form": form})
+
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
